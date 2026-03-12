caae74ec3855288646f2015f877493d60bd71a91
Andrew Wen <Andrew.Wen@uth.tmc.edu>
2026-03-12 19:56:19 +0000

    Scala ArraySeq used is of the immutable variant

--- src/main/java/net/openhealthkg/graphminer/edgeminer/EdgeMiner.java
+++ src/main/java/net/openhealthkg/graphminer/edgeminer/EdgeMiner.java
@@ -31,7 +31,7 @@ import org.apache.spark.sql.types.StructType;
 
 import com.azure.ai.openai.models.Embeddings;
 import com.azure.ai.openai.models.EmbeddingsOptions;
-import scala.collection.mutable.ArraySeq;
+import scala.collection.immutable.ArraySeq;
 import scala.jdk.CollectionConverters;
 
 import java.util.*;
